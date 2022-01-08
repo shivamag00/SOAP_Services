a70ff3b62fd617c6592e6e688e4afc556e5d19ea
Shivam Agarwal <shivam.agrawal.2000@gmail.com>
2022-01-09 00:40:04 +0530

    first commit

---
+++ src/main/java/com/example/webservices/soapcoursemanagement/soap/CourseDetailsEndpoint.java
@@ -0,0 +1,76 @@
+package com.example.webservices.soapcoursemanagement.soap;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import com.example.courses.CourseDetails;
+import com.example.courses.GetAllCourseDetailsRequest;
+import com.example.courses.GetAllCourseDetailsResponse;
+import com.example.courses.GetCourseDetailsRequest;
+import com.example.courses.GetCourseDetailsResponse;
+import com.example.webservices.soapcoursemanagement.bean.Course;
+import com.example.webservices.soapcoursemanagement.service.CourseDetailsService;
+
+@Endpoint
+public class CourseDetailsEndpoint {
+
+	@Autowired
+	CourseDetailsService service;
+
+	// method
+	// input - GetCourseDetailsRequest
+	// output - GetCourseDetailsResponse
+
+	// http://in28minutes.com/courses
+	// GetCourseDetailsRequest
+	@PayloadRoot(namespace = "http://example.com/courses", localPart = "GetCourseDetailsRequest")
+	@ResponsePayload
+	public GetCourseDetailsResponse processCourseDetailsRequest(@RequestPayload GetCourseDetailsRequest request) {
+		
+		Course course = service.findById(request.getId());
+
+		return mapCourseDetails(course);
+	}
+	
+	@PayloadRoot(namespace = "http://example.com/courses", localPart = "GetAllCourseDetailsRequest")
+	@ResponsePayload
+	public GetAllCourseDetailsResponse processAllCourseDetailsRequest(
+			@RequestPayload GetAllCourseDetailsRequest request) {
+
+		List<Course> courses = service.findAll();
+
+		return mapAllCourseDetails(courses);
+	}
+	
+	private GetCourseDetailsResponse mapCourseDetails(Course course) {
+		GetCourseDetailsResponse response = new GetCourseDetailsResponse();
+		response.setCourseDetails(mapCourse(course));
+		return response;
+	}
+	
+	private GetAllCourseDetailsResponse mapAllCourseDetails(List<Course> courses) {
+		GetAllCourseDetailsResponse response = new GetAllCourseDetailsResponse();
+		for (Course course : courses) {
+			CourseDetails mapCourse = mapCourse(course);
+			response.getCourseDetails().add(mapCourse);
+		}
+		return response;
+	}
+	
+	private CourseDetails mapCourse(Course course) {
+		CourseDetails courseDetails = new CourseDetails();
+
+		courseDetails.setId(course.getId());
+
+		courseDetails.setName(course.getName());
+
+		courseDetails.setDescription(course.getDescription());
+		return courseDetails;
+	}
+	
+}
